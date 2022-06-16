b4d5dbc9b40f0237812c28ae19c48ea4892facf7
ulzcq <syj9913@ynu.ac.kr>
2022-06-17 03:04:28 +0900

    Feat: 간단한 주문 조회 V4- JPA에서 DTO로 바로 조회(1)
    [API 개발 고급] - 지연 로딩과 조회 성능 최적화
    V3-페치 조인, V4-JPQL로 콕 집어서 조회하기
    두 가지 방법의 장단점을 알아보는 학습
    
    Issues: #4

---
+++ src/main/java/jpabook/jpashop/repository/OrderSimpleQueryDto.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderSimpleQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    //Dto가 엔티티를 받는 것은 크게 문제가 되지 않음
+    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        //jpql에서 엔티티를 넘기면 식별자로 넣기 때문에 일일히 파라미터로 받아야함
+        //address는 값처럼 동작하므로 ㄱㅊ음
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
