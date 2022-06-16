7043d736e8dc41e18e932495af3bfb362f0f7d1f
ulzcq <syj9913@ynu.ac.kr>
2022-06-16 17:20:40 +0900

    Fix: Address 필드에 @JsonIgnore 빠뜨림

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,6 +20,7 @@ public class Member {
     @NotEmpty
     private String name;
 
+    @JsonIgnore
     @Embedded
     private Address address;
 
