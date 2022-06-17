6b7a1f38a46484d0e1a19d4eb6cb02b76661c166
ulzcq <syj9913@ynu.ac.kr>
2022-06-17 19:50:46 +0900

    Feat: 주문 조회 V2- 엔티티를 DTO로 변환
    [API 개발 고급] - 컬렉션 조회 최적화
    V2 메서드 작성. 그런데 이렇게 하면 안된다 !
    
    Issues: #5

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,19 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +34,39 @@ public class OrderApiController {
             orderItems.stream().forEach(o -> o.getItem().getName()); //orderItem 내부의 item의 이름도 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        //DTO 변환
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return collect;
+    }
+
+    @Getter //@Data는 toString까지 만들어주기도하고, 그냥 Getter 쓰는게 나을 수도.
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItem> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+
+            order.getOrderItems().stream().forEach(o -> o.getItem().getName()); //LAZY 초기화
+            orderItems = order.getOrderItems();
+        }
     }
 }
