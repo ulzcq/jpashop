3f2e8a83aa607499bd9aeb8b78af86f60e53ad5e
ulzcq <syj9913@ynu.ac.kr>
2022-06-24 21:02:13 +0900

    Feat: 주문 조회 V6- 플랫 데이터 최적화
    [API 개발 고급] - 컬렉션 조회 최적화
    모두 조인해서 쿼리 한방으로 조회
    
    Issues: #8

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
@@ -34,6 +35,9 @@ import java.util.stream.Collectors;
  *
  * V5. JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1 + 1 Query)
  * - 페이징 가능
+ *
+ * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1 Query) (1 Query)
+ * - 페이징 불가능...
  */
 @RestController
 @RequiredArgsConstructor
@@ -124,7 +128,11 @@ public class OrderApiController {
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5(){
         return orderQueryRepository.findAllByDto_optimization();
+    }
 
+    @GetMapping("/api/v6/orders")
+    public List<OrderFlatDto> ordersV6(){
+        return orderQueryRepository.findAllByDto_flat();
     }
 
     @Getter //@Data는 toString까지 만들어주기도하고, 그냥 Getter 쓰는게 나을 수도.
